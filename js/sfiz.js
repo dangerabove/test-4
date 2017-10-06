9ef3ef205bdc3b5597bfe8c311ab21e55b690400
dangerabove <ccjxd@wp.pl>
2017-10-06 14:33:12 +0200

    Update sfiz.js

--- js/sfiz.js
+++ js/sfiz.js
@@ -10,7 +10,7 @@ s_fizState.prototype = {
 	else if(45 * dif <= scrfiz) {scr = 4;}
 	else if(25 * dif <= scrfiz) {scr = 3;}
 	else if(10 * dif <= scrfiz) {scr = 2;}
-	else if(0  * dif <= scrfiz) {scr = 1;}
+	else {scr = 1;}
 
     var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
     text = game.add.text(80, 0, "Twoja ocena: " + scr, style);
